2e304a97a98c0aefd0af4385394d75078e92023f
Eric Kennedy <ekeric13@gmail.com>
2015-01-22 20:32:38 -0800

    Add create user function in router

--- client/app/router.js
+++ client/app/router.js
@@ -3,16 +3,23 @@ Lernhow.Router = Backbone.Router.extend({
     this.$el = options.el;
   },
   routes: {
-    '': 'index'
+    '': 'index',
+    'create': 'createUser'
   },
   swapView: function(view) {
     this.$el.html(view.render().el);
   },
   index: function() {
     // get all guides
+      // var guides = new Lernhow.guides();
     // init guidesView
+      // var guidesView = new Lernhow.GuidesView({collection: guides})
     // swap the guidesView into place
-    // this.swapView(guidesView);
-    alert("Index Route hit on Client");
+      // this.swapView(guidesView);
+    console.log("Index Route hit on Client");
+  },
+
+  createUser: function(){
+    this.swapView(new Lernhow.createUserView())
   }
 });
